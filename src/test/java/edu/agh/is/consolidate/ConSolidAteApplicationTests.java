2a43e66866f7a0340043753956de723da0fa43d6
vis4rd <alagaesia.vrael@gmail.com>
2024-06-15 10:57:08 +0200

    Add ConSOLIDate app as base

---
+++ src/test/java/edu/agh/is/consolidate/ConSolidAteApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.agh.is.consolidate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConSolidAteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
