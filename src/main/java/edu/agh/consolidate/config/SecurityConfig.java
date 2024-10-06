42e5bebb1a74cfeb2b28dc2c26021c394194d860
laseka <aga.lasek55@gmail.com>
2024-10-06 23:45:10 +0200

    Updated project

--- src/main/java/edu/agh/consolidate/config/SecurityConfig.java
+++ src/main/java/edu/agh/consolidate/config/SecurityConfig.java
@@ -1,38 +1,58 @@
 package edu.agh.consolidate.config;
 
-import edu.agh.consolidate.controller.LogoutHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.core.oidc.endpoint.OidcParameterNames;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final LogoutHandler logoutHandler;
-
-    public SecurityConfig(LogoutHandler logoutHandler) {
-        this.logoutHandler = logoutHandler;
-    }
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/")
-                        .permitAll()
-                        .anyRequest()
-                        .authenticated())
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                            .requestMatchers("/").permitAll()
+                            .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 ->
+                        oauth2
+                            .authorizationEndpoint(authorization -> authorization
+                                    .authorizationRequestResolver(
+                                            requestResolver(this.clientRegistrationRepository)
+                                    )
+                            )
+                            .defaultSuccessUrl("/profile", true)
+                )
                 .logout(logout -> logout
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                        .addLogoutHandler(logoutHandler))
-                .formLogin(Customizer.withDefaults())
-                .oauth2Login(Customizer.withDefaults())
-                .build();
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .deleteCookies("JSESSIONID")
+                );
+        return http.build();
     }
-
+    private static OAuth2AuthorizationRequestResolver requestResolver
+            (ClientRegistrationRepository clientRegistrationRepository) {
+        DefaultOAuth2AuthorizationRequestResolver requestResolver =
+                new DefaultOAuth2AuthorizationRequestResolver(clientRegistrationRepository,
+                        "/oauth2/authorization");
+        requestResolver.setAuthorizationRequestCustomizer(config ->
+                config.attributes(stringObjectMap -> stringObjectMap.remove(OidcParameterNames.NONCE))
+                        .parameters(params -> params.remove(OidcParameterNames.NONCE))
+        );
+        return requestResolver;
+    }
 }
